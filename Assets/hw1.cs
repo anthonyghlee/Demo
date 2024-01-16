189c0ebd1206e4fab041df66002af97423d922d8
Anthony Lee <anthonyghlee@g.ucla.edu>
2024-01-15 22:22:30 -0800

    idk

--- Assets/hw1.cs
+++ Assets/hw1.cs
@@ -7,7 +7,13 @@ public class hw1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int score = 20
         
+        if(score > 18){
+            Debug.Log(You win!)
+        }else{
+            Debug.Log(You lose)
+        }
     }
 
     // Update is called once per frame
